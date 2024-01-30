a7fd004a5c5f5671a03618dfc643b878842fc039
tiq0015 <tiq0015@naver.com>
2024-01-31 01:45:20 +0900

    .

--- librarySQL/library.sql
+++ librarySQL/library.sql
@@ -1,7 +1,4 @@
 
-create user 'library'@'%' identified by 'library';
-grant all privileges on *.* to 'library'@'%';
-
 create database library_db
   default character set utf8mb4
     collate utf8mb4_general_ci
@@ -34,9 +31,18 @@ create table library(
     references member (member_id)
     );
     
-insert into library values (null, 'firstJAVA', '유영진', 'YD', '2024-01-01', 'falst', '2024-01-30', null);
-select book_id,
-	   title
-from library;
+insert into library values (null, 'firstJAVA', '유영진', 'YD', '2024-01-01', '대여가능', '2024-01-30', null);
+select * from library;
+select * from member;
     
-show tables;
+update library 
+   set title = '조선왕조실록',
+	   author_name = '박영규',
+	   pubs = '웅진',
+	   pub_date = null,
+	   rent = '대여가능',
+ 	   rent_date = null,
+ 	   member_id = null
+ where book_id = 3;
+show tables;update book     set title = '조선왕조실록',     author_name = '박영규',     pubs = '웅진',     pub_date = null,     rent = '대여가능',      rent_date = null,      member_id = null  where book_id = 3
+update book     set title = '조선왕조실록',     author_name = '박영규',     pubs = '웅진',     pub_date = null,     rent = '대여가능',      rent_date = null,      member_id = null  where book_id = 3
