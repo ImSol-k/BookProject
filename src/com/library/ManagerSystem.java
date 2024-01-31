09476153dc73a5d401f9305f3e5bfa8182d875b9
pjh990909 <pjh990909@naver.com>
2024-01-31 18:36:36 +0900

    rent

--- src/com/library/ManagerSystem.java
+++ src/com/library/ManagerSystem.java
@@ -1,5 +1,115 @@
 package com.library;
 
+import java.util.Scanner;
+
 public class ManagerSystem {
+
+	Scanner sc = new Scanner(System.in);
+
+	public void managerMain() {
+
+		boolean go = true;
+
+		while (go) {
+
+			System.out.println("");
+			System.out.println("");
+			System.out.println("");
+			System.out.println("");
+			System.out.println("===============================");
+			System.out.println("1.대여관리|2.회원관리|3.책관리|4.종료");
+			System.out.println("===============================");
+			System.out.print("입력란:");
+
+			int choose = sc.nextInt();
+
+			switch (choose) {
+
+					case 1:
+						// 대여관리
+						rentManagement();
+						break;
+
+					case 2:
+						// 회원관리
+						
+						break;
+						
+					case 3:
+						//책관리
+						
+						break;
+						
+					case 4:
+						//종료
+						go = false;
+						break;
+						
+					default:
+						System.out.println("다시 입력해주세요");
+
+			}// choose
+
+		} // go
+
+	}//managerMain
+	
+	public void rentManagement() {
+		
+		boolean lo = true;
+		
+		while(lo) {
+			System.out.println("");
+			System.out.println("");
+			System.out.println("");
+			System.out.println("");
+			System.out.println("==================================");
+			System.out.println("1.대여|2.반납|3.대여현황|4.돌아가기|5.종료");
+			System.out.println("==================================");
+			System.out.print("입력란:");
+			
+			int rm = sc.nextInt();
+			
+			switch (rm) {
+
+			case 1:
+				// 대여
+				
+				break;
+
+			case 2:
+				// 반납
+				
+				break;
+				
+			case 3:
+				//대여현황
+				
+				break;
+				
+			case 4:
+				//돌아가기
+				
+				break;
+				
+			case 5:
+				//종료
+				lo = false;
+				break;
+				
+			default:
+				System.out.println("다시 입력해주세요");
+
+			}//rm
+			
+		}//lo
+		
+		
+		
+		
+		
+	}//rentManament
+	
+	
 
 }
--- src/com/library/RentTest.java
+++ src/com/library/RentTest.java
@@ -5,11 +5,15 @@ public class RentTest {
 	public static void main(String[] args) {
 		
 		RentDao rentDao = new RentDao();
+		RentVo rentVo = new RentVo();
 		
-		rentDao.rentSelect();
+		//rentDao.rentSelect();
 		
+		//rentDao.rentInsert(rentVo);
 		
+		//rentDao.rentDelete(2);
 		
+		rentDao.rentUpdate(rentVo);
 
 	}
 
