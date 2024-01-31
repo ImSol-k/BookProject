b2b611f16b305f8569e0a6cc90c23f0ca7a33cf2
tiq0015 <tiq0015@naver.com>
2024-01-31 11:15:02 +0900

    create

---
+++ librarySQL/Create.sql
@@ -0,0 +1,90 @@
+
+create database library_db
+  default character set utf8mb4
+    collate utf8mb4_general_ci
+    default encryption='n';
+
+
+CREATE TABLE `librarys` (
+	`book_id`	int	NOT NULL,
+	`rent`	varchar(10)	NOT NULL,
+	`title`	varchar(20)	NOT NULL,
+	`author`	varchar(10)	NULL,
+	`pubs`	varchar(20)	NULL,
+	`pub_date`	date	NULL
+);
+
+CREATE TABLE `members` (
+	`member_num`	int	NOT NULL,
+	`book_id`	int	NOT NULL,
+	`rent`	varchar(10)	NOT NULL,
+	`member_id`	varchar(20)	NOT NULL,
+	`member_pw`	varchar(20)	NOT NULL,
+	`name`	varchar(30)	NOT NULL,
+	`ph`	varchar(20)	NULL,
+	`address`	varchar(30)	NULL
+);
+
+CREATE TABLE `rents` (
+	`rent`	varchar(10)	NOT NULL,
+	`book_id`	int	NOT NULL,
+	`member_num`	int	NOT NULL,
+	`rent_date`	date	NULL,
+	`Field`	date	NULL
+);
+
+ALTER TABLE `librarys` ADD CONSTRAINT `PK_LIBRARYS` PRIMARY KEY (
+	`book_id`,
+	`rent`
+);
+
+ALTER TABLE `members` ADD CONSTRAINT `PK_MEMBERS` PRIMARY KEY (
+	`member_num`,
+	`book_id`,
+	`rent`
+);
+
+ALTER TABLE `rents` ADD CONSTRAINT `PK_RENTS` PRIMARY KEY (
+	`rent`,
+	`book_id`
+);
+
+ALTER TABLE `librarys` ADD CONSTRAINT `FK_rents_TO_librarys_1` FOREIGN KEY (
+	`rent`
+)
+REFERENCES `rents` (
+	`rent`
+);
+
+ALTER TABLE `members` ADD CONSTRAINT `FK_librarys_TO_members_1` FOREIGN KEY (
+	`book_id`
+)
+REFERENCES `librarys` (
+	`book_id`
+);
+
+ALTER TABLE `members` ADD CONSTRAINT `FK_librarys_TO_members_2` FOREIGN KEY (
+	`rent`
+)
+REFERENCES `librarys` (
+	`rent`
+);
+
+ALTER TABLE `rents` ADD CONSTRAINT `FK_librarys_TO_rents_1` FOREIGN KEY (
+	`book_id`
+)
+REFERENCES `librarys` (
+	`book_id`
+);
+
+ALTER TABLE `rents` ADD CONSTRAINT `FK_members_TO_rents_1` FOREIGN KEY (
+	`member_num`
+)
+REFERENCES `members` (
+	`member_num`
+);
+
+show databases;
+show tables;
+
+
--- librarySQL/library.sql
+++ librarySQL/library.sql
@@ -1,48 +1,20 @@
 
-create database library_db
-  default character set utf8mb4
-    collate utf8mb4_general_ci
-    default encryption='n';
 
-show databases;
-show tables;
 
-drop table library;
-drop table member;
 
-create table member(
-	member_id	int			auto_increment	primary key,
-    member_pw	varchar(30) not null,
-    member_name varchar(30) not null,
-    ph varchar(30),
-    address varchar(30)
-    );
-
-create table library(
-	book_id	int			auto_increment	primary key,
-    title	varchar(30) not null,
-    author_name varchar(30),
-    pubs varchar(30),
-    pub_date varchar(30),
-    rent varchar(10),
-    rent_date date,
-    member_id int,
-    constraint library_fk foreign key (member_id)
-    references member (member_id)
-    );
+drop table librarys;
+drop table members;
+drop table rents;
     
-insert into library values (null, 'firstJAVA', '유영진', 'YD', '2024-01-01', '대여가능', '2024-01-30', null);
-select * from library;
-select * from member;
+  
+
+select * from librarys;
+select * from members;
     
 update library 
    set title = '조선왕조실록',
 	   author_name = '박영규',
 	   pubs = '웅진',
 	   pub_date = null,
-	   rent = '대여가능',
- 	   rent_date = null,
  	   member_id = null
  where book_id = 3;
-show tables;update book     set title = '조선왕조실록',     author_name = '박영규',     pubs = '웅진',     pub_date = null,     rent = '대여가능',      rent_date = null,      member_id = null  where book_id = 3
-update book     set title = '조선왕조실록',     author_name = '박영규',     pubs = '웅진',     pub_date = null,     rent = '대여가능',      rent_date = null,      member_id = null  where book_id = 3
